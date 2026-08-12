4f03d421197b9afa84f87458d3f3247eef0489b3
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 01:00:50 +0300

    Publish Blazor standalone project file: Opportunities.razor.designer.cs

---
+++ client/Pages/Opportunities.razor.designer.cs
@@ -0,0 +1,189 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using BlazorCrmWasm.Models.Crm;
+using BlazorCrmWasm.Client.Pages;
+
+namespace BlazorCrmWasm.Pages
+{
+    public partial class OpportunitiesComponent : ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, dynamic> Attributes { get; set; }
+
+        public void Reload()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+        }
+
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        protected CrmService Crm { get; set; }
+        protected RadzenDataGrid<BlazorCrmWasm.Models.Crm.Opportunity> grid0;
+
+        string _search;
+        protected string search
+        {
+            get
+            {
+                return _search;
+            }
+            set
+            {
+                if (!object.Equals(_search, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "search", NewValue = value, OldValue = _search };
+                    _search = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        IEnumerable<BlazorCrmWasm.Models.Crm.Opportunity> _getOpportunitiesResult;
+        protected IEnumerable<BlazorCrmWasm.Models.Crm.Opportunity> getOpportunitiesResult
+        {
+            get
+            {
+                return _getOpportunitiesResult;
+            }
+            set
+            {
+                if (!object.Equals(_getOpportunitiesResult, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "getOpportunitiesResult", NewValue = value, OldValue = _getOpportunitiesResult };
+                    _getOpportunitiesResult = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        int _getOpportunitiesCount;
+        protected int getOpportunitiesCount
+        {
+            get
+            {
+                return _getOpportunitiesCount;
+            }
+            set
+            {
+                if (!object.Equals(_getOpportunitiesCount, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "getOpportunitiesCount", NewValue = value, OldValue = _getOpportunitiesCount };
+                    _getOpportunitiesCount = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+            await Load();
+        }
+        protected async System.Threading.Tasks.Task Load()
+        {
+            if (string.IsNullOrEmpty(search)) {
+                search = "";
+            }
+        }
+
+        protected async System.Threading.Tasks.Task Button0Click(MouseEventArgs args)
+        {
+            var dialogResult = await DialogService.OpenAsync<AddOpportunity>("Add Opportunity", null);
+            await grid0.Reload();
+
+            await InvokeAsync(() => { StateHasChanged(); });
+        }
+
+        protected async System.Threading.Tasks.Task Splitbutton0Click(RadzenSplitButtonItem args)
+        {
+            if (args?.Value == "csv")
+            {
+                await Crm.ExportOpportunitiesToCSV(new Query() { Filter = $@"{(string.IsNullOrEmpty(grid0.Query.Filter)? "true" : grid0.Query.Filter)}", OrderBy = $"{grid0.Query.OrderBy}", Expand = "Contact,OpportunityStatus", Select = "Id,Amount,UserId,Contact.Email as ContactEmail,OpportunityStatus.Name as OpportunityStatusName,CloseDate,Name" }, $"Opportunities");
+
+            }
+
+            if (args == null || args.Value == "xlsx")
+            {
+                await Crm.ExportOpportunitiesToExcel(new Query() { Filter = $@"{(string.IsNullOrEmpty(grid0.Query.Filter)? "true" : grid0.Query.Filter)}", OrderBy = $"{grid0.Query.OrderBy}", Expand = "Contact,OpportunityStatus", Select = "Id,Amount,UserId,Contact.Email as ContactEmail,OpportunityStatus.Name as OpportunityStatusName,CloseDate,Name" }, $"Opportunities");
+
+            }
+        }
+
+        protected async System.Threading.Tasks.Task Grid0LoadData(LoadDataArgs args)
+        {
+            try
+            {
+                var crmGetOpportunitiesResult = await Crm.GetOpportunities(filter:$@"(contains(UserId,""{search}"") or contains(Name,""{search}"")) and {(string.IsNullOrEmpty(args.Filter)? "true" : args.Filter)}", orderby:$"{args.OrderBy}", expand:$"Contact,OpportunityStatus", top:args.Top, skip:args.Skip, count:args.Top != null && args.Skip != null);
+                getOpportunitiesResult = crmGetOpportunitiesResult.Value.AsODataEnumerable();
+
+                getOpportunitiesCount = crmGetOpportunitiesResult.Count;
+            }
+            catch (System.Exception crmGetOpportunitiesException)
+            {
+                NotificationService.Notify(new NotificationMessage(){ Severity = NotificationSeverity.Error,Summary = $"Error",Detail = $"Unable to load Opportunities" });
+            }
+        }
+
+        protected async System.Threading.Tasks.Task Grid0RowDoubleClick(DataGridRowMouseEventArgs<BlazorCrmWasm.Models.Crm.Opportunity> args)
+        {
+            var dialogResult = await DialogService.OpenAsync<EditOpportunity>("Edit Opportunity", new Dictionary<string, object>() { {"Id", args.Data.Id} });
+            await grid0.Reload();
+
+            await InvokeAsync(() => { StateHasChanged(); });
+        }
+
+        protected async System.Threading.Tasks.Task GridDeleteButtonClick(MouseEventArgs args, dynamic data)
+        {
+            try
+            {
+                if (await DialogService.Confirm("Are you sure you want to delete this record?") == true)
+                {
+                    var crmDeleteOpportunityResult = await Crm.DeleteOpportunity(id:data.Id);
+                    if (crmDeleteOpportunityResult != null && crmDeleteOpportunityResult.StatusCode == System.Net.HttpStatusCode.NoContent)
+                    {
+                        await grid0.Reload();
+                    }
+
+                    if (crmDeleteOpportunityResult != null && crmDeleteOpportunityResult.StatusCode != System.Net.HttpStatusCode.NoContent)
+                    {
+                        NotificationService.Notify(new NotificationMessage(){ Severity = NotificationSeverity.Error,Summary = $"Error",Detail = $"Unable to delete Opportunity" });
+                    }
+                }
+            }
+            catch (System.Exception crmDeleteOpportunityException)
+            {
+                NotificationService.Notify(new NotificationMessage(){ Severity = NotificationSeverity.Error,Summary = $"Error",Detail = $"Unable to delete Opportunity" });
+            }
+        }
+    }
+}
