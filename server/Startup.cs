9c88b25d9dffce84fde02faab954371b2aabc762
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 01:03:56 +0300

    Publish Blazor standalone project file: Startup.cs

---
+++ server/Startup.cs
@@ -0,0 +1,132 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.EntityFrameworkCore;
+
+using BlazorCrmWasm.Data;
+
+using Microsoft.OData.ModelBuilder;
+using Microsoft.AspNetCore.OData;
+
+namespace BlazorCrmWasm
+{
+    public partial class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        partial void OnConfigureServices(IServiceCollection services);
+
+        partial void OnConfiguringServices(IServiceCollection services);
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            OnConfiguringServices(services);
+
+            services.AddHttpContextAccessor();
+            services.AddCors(options =>
+            {
+                options.AddPolicy(
+                    "AllowAny",
+                    x =>
+                    {
+                        x.AllowAnyHeader()
+                        .AllowAnyMethod()
+                        .SetIsOriginAllowed(isOriginAllowed: _ => true)
+                        .AllowCredentials();
+                    });
+            });
+            var oDataBuilder = new ODataConventionModelBuilder();
+
+            oDataBuilder.EntitySet<BlazorCrmWasm.Models.Crm.Contact>("Contacts");
+            oDataBuilder.EntitySet<BlazorCrmWasm.Models.Crm.Opportunity>("Opportunities");
+            oDataBuilder.EntitySet<BlazorCrmWasm.Models.Crm.OpportunityStatus>("OpportunityStatuses");
+            oDataBuilder.EntitySet<BlazorCrmWasm.Models.Crm.SchedulerAppointment>("SchedulerAppointments");
+            oDataBuilder.EntitySet<BlazorCrmWasm.Models.Crm.TaskStatus>("TaskStatuses");
+            oDataBuilder.EntitySet<BlazorCrmWasm.Models.Crm.TaskType>("TaskTypes");
+            oDataBuilder.EntitySet<BlazorCrmWasm.Models.Crm.Tasklist>("Tasklists");
+
+            this.OnConfigureOData(oDataBuilder);
+
+
+            var model = oDataBuilder.GetEdmModel();
+            services.AddControllers().AddOData(opt => { 
+              opt.AddRouteComponents("odata/CRM", model).Count().Filter().OrderBy().Expand().Select().SetMaxTop(null).TimeZone = TimeZoneInfo.Utc;
+            });
+
+            
+
+            services.AddDbContext<BlazorCrmWasm.Data.CrmContext>(options =>
+            {
+              options.UseSqlServer(Configuration.GetConnectionString("CRMConnection"));
+            });
+
+            services.AddRazorPages();
+
+            OnConfigureServices(services);
+        }
+
+        partial void OnConfigure(IApplicationBuilder app, IWebHostEnvironment env);
+        partial void OnConfigureOData(ODataConventionModelBuilder builder);
+        partial void OnConfiguring(IApplicationBuilder app, IWebHostEnvironment env);
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            OnConfiguring(app, env);
+            if (env.IsDevelopment())
+            {
+                Microsoft.IdentityModel.Logging.IdentityModelEventSource.ShowPII = true;
+                app.UseDeveloperExceptionPage();
+                app.UseWebAssemblyDebugging();
+            }
+            else
+            {
+                app.Use((ctx, next) =>
+                {
+                    ctx.Request.Scheme = "https";
+                    return next();
+                });
+            }
+            app.UseHttpsRedirection();
+            app.UseBlazorFrameworkFiles();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            IServiceProvider provider = app.ApplicationServices.GetRequiredService<IServiceProvider>();
+            app.UseCors("AllowAny");
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "default",
+                  pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapRazorPages();
+                endpoints.MapControllers();
+                endpoints.MapFallbackToFile("index.html");
+            });
+
+            OnConfigure(app, env);
+        }
+    }
+
+
+}
