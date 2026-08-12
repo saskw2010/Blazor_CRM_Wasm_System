c3ca5def8d6cf5dd102e2a0a6a7968db17f35c2b
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 01:00:36 +0300

    Publish Blazor standalone project file: EditTaskStatus.razor.designer.cs

---
+++ client/Pages/EditTaskStatus.razor.designer.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using BlazorCrmWasm.Models.Crm;
+using BlazorCrmWasm.Client.Pages;
+
+namespace BlazorCrmWasm.Pages
+{
+    public partial class EditTaskStatusComponent : ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, dynamic> Attributes { get; set; }
+
+        public void Reload()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+        }
+
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        protected CrmService Crm { get; set; }
+
+        [Parameter]
+        public dynamic Id { get; set; }
+
+        bool _hasChanges;
+        protected bool hasChanges
+        {
+            get
+            {
+                return _hasChanges;
+            }
+            set
+            {
+                if (!object.Equals(_hasChanges, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "hasChanges", NewValue = value, OldValue = _hasChanges };
+                    _hasChanges = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        bool _canEdit;
+        protected bool canEdit
+        {
+            get
+            {
+                return _canEdit;
+            }
+            set
+            {
+                if (!object.Equals(_canEdit, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "canEdit", NewValue = value, OldValue = _canEdit };
+                    _canEdit = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        BlazorCrmWasm.Models.Crm.TaskStatus _taskstatus;
+        protected BlazorCrmWasm.Models.Crm.TaskStatus taskstatus
+        {
+            get
+            {
+                return _taskstatus;
+            }
+            set
+            {
+                if (!object.Equals(_taskstatus, value))
+                {
+                    var args = new PropertyChangedEventArgs(){ Name = "taskstatus", NewValue = value, OldValue = _taskstatus };
+                    _taskstatus = value;
+                    OnPropertyChanged(args);
+                    Reload();
+                }
+            }
+        }
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+            await Load();
+        }
+        protected async System.Threading.Tasks.Task Load()
+        {
+            hasChanges = false;
+
+            canEdit = true;
+
+            var crmGetTaskStatusByIdResult = await Crm.GetTaskStatusById(id:Id);
+            taskstatus = crmGetTaskStatusByIdResult;
+
+            canEdit = crmGetTaskStatusByIdResult != null;
+        }
+
+        protected async System.Threading.Tasks.Task CloseButtonClick(MouseEventArgs args)
+        {
+            DialogService.Close(null);
+        }
+
+        protected async System.Threading.Tasks.Task Button0Click(MouseEventArgs args)
+        {
+            await this.Load();
+        }
+
+        protected async System.Threading.Tasks.Task Form0Submit(BlazorCrmWasm.Models.Crm.TaskStatus args)
+        {
+            try
+            {
+                var crmUpdateTaskStatusResult = await Crm.UpdateTaskStatus(id:Id, taskStatus:taskstatus);
+                if (crmUpdateTaskStatusResult.StatusCode != System.Net.HttpStatusCode.PreconditionFailed) {
+                  DialogService.Close(taskstatus);
+                }
+
+                hasChanges = crmUpdateTaskStatusResult.StatusCode == System.Net.HttpStatusCode.PreconditionFailed;
+            }
+            catch (System.Exception crmUpdateTaskStatusException)
+            {
+                NotificationService.Notify(new NotificationMessage(){ Severity = NotificationSeverity.Error,Summary = $"Error",Detail = $"Unable to update TaskStatus" });
+
+            hasChanges = crmUpdateTaskStatusException.Message.Contains("412");
+
+            if (!crmUpdateTaskStatusException.Message.Contains("412")) {
+                canEdit = false;
+            }
+            }
+        }
+
+        protected async System.Threading.Tasks.Task Button4Click(MouseEventArgs args)
+        {
+            DialogService.Close(null);
+        }
+    }
+}
