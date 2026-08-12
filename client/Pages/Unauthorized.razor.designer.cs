2f42e0dec688f4e04cdabeb90e05f2c473d9c146
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 01:01:31 +0300

    Publish Blazor standalone project file: Unauthorized.razor.designer.cs

---
+++ client/Pages/Unauthorized.razor.designer.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.JSInterop;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.Web;
+using Radzen;
+using Radzen.Blazor;
+using BlazorCrmWasm.Models.Crm;
+using Microsoft.AspNetCore.Identity;
+using BlazorCrmWasm.Models;
+using BlazorCrmWasm.Client.Pages;
+
+namespace BlazorCrmWasm.Pages
+{
+    public partial class UnauthorizedComponent : ComponentBase
+    {
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IReadOnlyDictionary<string, dynamic> Attributes { get; set; }
+
+        public void Reload()
+        {
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void OnPropertyChanged(PropertyChangedEventArgs args)
+        {
+        }
+
+        [Inject]
+        protected IJSRuntime JSRuntime { get; set; }
+
+        [Inject]
+        protected NavigationManager UriHelper { get; set; }
+
+        [Inject]
+        protected DialogService DialogService { get; set; }
+
+        [Inject]
+        protected TooltipService TooltipService { get; set; }
+
+        [Inject]
+        protected ContextMenuService ContextMenuService { get; set; }
+
+        [Inject]
+        protected NotificationService NotificationService { get; set; }
+
+        [Inject]
+        protected SecurityService Security { get; set; }
+
+        [Inject]
+        protected AuthenticationStateProvider AuthenticationStateProvider { get; set; }
+
+        [Inject]
+        protected CrmService Crm { get; set; }
+
+        protected override async System.Threading.Tasks.Task OnInitializedAsync()
+        {
+            await Security.InitializeAsync(AuthenticationStateProvider);
+        }
+    }
+}
