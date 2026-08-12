2f805a5b844ffa94484f0febb7740af11a27f138
Mostafa Elnagar <saskw2010@gmail.com>
2026-08-13 01:00:25 +0300

    Publish Blazor standalone project file: EditSchedulerAppointment.razor.cs

---
+++ client/Pages/EditSchedulerAppointment.razor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Radzen;
+using Radzen.Blazor;
+
+namespace BlazorCrmWasm.Pages
+{
+    public partial class EditSchedulerAppointmentComponent
+    {
+
+    }
+}
